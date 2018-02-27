3474571de3a2b05fdeba54e39d9311445fa2c25b
David Alejandro Marin Alzate <jandro240@gmail.com>
2018-02-27 15:31:35 -0500

    add database squema

---
+++ squema_database.sql
@@ -0,0 +1,36 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ * Author:  alejandro
+ * Created: feb 27, 2018
+ */
+
+/*
+    DATABASE NAME: dbmatriculas
+*/
+
+
+CREATE TABLE estudiante(
+    documento int PRIMARY KEY, 
+    nombre VARCHAR(50) NOT NULL, 
+    apellido VARCHAR(50) NOT NULL,
+    usuario VARCHAR(50) NOT NULL , 
+    contraseña VARCHAR(50) NOT NULL,
+    foto BLOB
+);
+
+CREATE TABLE materia( 
+    id_materia int PRIMARY KEY, 
+    nombre_materia VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE matricula( 
+    id_estudiante int , 
+    id_materia int ,
+    FOREIGN KEY (id_estudiante) REFERENCES estudiante(documento), 
+    FOREIGN KEY (id_materia) REFERENCES materia(id_materia),
+    PRIMARY KEY (id_estudiante, id_materia)
+);
